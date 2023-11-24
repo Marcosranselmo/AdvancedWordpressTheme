9c9c92e6450538508f59ea5c07a50fc0313cc9ef
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2023-11-23 22:56:59 -0300

    initial settings

---
+++ wp-content/themes/aquila/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Main template file.
+ * 
+ * @pachkage Aquila
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WordPress theme</title>
+</head>
+<body>
+    Hello WordPress! 
+</body>
+</html>
