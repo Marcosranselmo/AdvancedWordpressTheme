61fa2b29d40ef099d5336c2d70ac6eb48d89ddae
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2023-12-08 20:37:00 -0300

    loop_posts_in_bootstrap

--- wp-content/themes/aquila/index.php
+++ wp-content/themes/aquila/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Main template file.
  * 
@@ -11,31 +12,56 @@ get_header();
 <div id="primary">
     <main id="main" class="site-main mt-5" role="main">
         <?php
-        if ( have_posts() ) {
-            ?>
+        if (have_posts()) {
+        ?>
             <div class="container">
                 <?php
-                if (is_home() && ! is_front_page() ) {
-                    ?>
+                if (is_home() && !is_front_page()) {
+                ?>
                     <header class="mb-5">
                         <h1 class="page-title screen-reader-text">
                             <?php single_post_title(); ?>
                         </h1>
                     </header>
-                    <?php 
-                    }
-                ?>
                 <?php
-                    while ( have_posts() ) : the_post();
-                    the_title();
-                    the_excerpt();
-                    endwhile;
+                }
                 ?>
+                <div class="row">
+                    <?php
+                    $index = 0;
+                    $no_of_columns = 3;
+
+                    // Case: index = 0;
+                    // Start the loop.
+                    while (have_posts()) : the_post();
+
+                        if ( 0 === $index % $no_of_columns ) {
+                        ?>
+                        <div class="col-lg-4 col-md-6 col-sm-12">
+                        <?php
+                        }
+                        ?>
+                        <h3><?php the_title(); ?></h3>
+                        <div><?php the_excerpt(); ?></div>
+                        <?php
+
+                        $index ++;
+                        // Index value = 2;
+
+                        if ( 0 !== $index && 0 === $index % $no_of_columns ) {
+                            ?>
+                            </div>
+                            <?php
+                        }
+
+                    endwhile;
+                        ?>
             </div>
-            <?php
-            }
+</div>
+        <?php
+        }
         ?>
-    </main>
+</main>
 </div>
 
 <?php get_footer(); ?>
