faed84b7ea030cfd02ae83a25d589c515f25c3a9
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2023-12-19 17:25:39 -0300

    verify_nonce

--- wp-content/themes/aquila/inc/classes/class-meta-boxes.php
+++ wp-content/themes/aquila/inc/classes/class-meta-boxes.php
@@ -44,6 +44,11 @@ class Meta_Boxes {
 
         $value = get_post_meta( $post->ID, '_hide_page_title', true );
 
+        /**
+         * Use nonce form verification
+         */
+        wp_nonce_field( plugin_basename(__FILE__), 'hide_title_meta_box_nonce_name' );
+
         ?>
         <label for="aquila-field"><?php esc_html_e( 'Hide the page title', 'aquila' ); ?></label>
         <select name="aquila_hide_title_field" id="aquila-field" class="postbox">
@@ -59,6 +64,24 @@ class Meta_Boxes {
     }
 
     public function save_post_meta_data( $post_id ) {
+
+        /**
+         * When the post is saved or updated we get $_POST available
+         * Check if the current user is authorized
+         */
+        if ( ! current_user_can( 'edit_post', $post_id ) ) {
+            return;
+        }
+
+        /**
+         * check if the nonce value we received is the same we created.
+         */
+        if ( ! isset( $_POST['hide_title_meta_box_nonce_name'] ) || 
+            ! wp_verify_nonce( $_POST['hide_title_meta_box_nonce_name'], plugin_basename(__FILE__) )
+        ) {
+            return;
+        }
+
         if (array_key_exists('aquila_hide_title_field', $_POST)) {
             update_post_meta(
                 $post_id,
