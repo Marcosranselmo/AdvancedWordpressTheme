d252f30090a31fd0fa551a3c69f0417271938df7
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2023-12-20 23:15:05 -0300

    get_post_date_and_time

--- wp-content/themes/aquila/inc/helpers/template-tags.php
+++ wp-content/themes/aquila/inc/helpers/template-tags.php
@@ -39,3 +39,26 @@ function get_the_post_custom_thumbnail( $post_id, $size = 'featured-thumbnail', 
 function the_post_custom_thumbnail( $post_id, $size = 'featured-thumbnail', $additional_attributes = [] ) {
     echo get_the_post_custom_thumbnail( $post_id, $size, $additional_attributes );
 }
+
+function aquila_posted_on() {
+    $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+
+    // Post is modified ( when post published time is not equal to post modified time )
+    if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+        $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+    }
+
+    $time_string = sprintf( $time_string,
+        esc_attr( get_the_date( DATE_W3C ) ),
+        esc_attr( get_the_date() ), 
+        esc_attr( get_the_modified_date( DATE_W3C ) ),
+        esc_attr( get_the_modified_date() )
+    );
+
+    $posted_on = sprintf(
+        esc_html_x( 'Posted on %s', 'post date', 'aquila' ),
+        '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+    );
+
+    echo '<span class="posted-one text-secondary">' . $posted_on . '</span>';
+}
---
+++ wp-content/themes/aquila/template-parts/components/blog/entry-meta.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template for entry meta
+ * 
+ * @pagkage Aquila
+ */
+
+?>
+
+<div class="entry-meta mb-3">
+    <?php aquila_posted_on(); ?>
+</div>
