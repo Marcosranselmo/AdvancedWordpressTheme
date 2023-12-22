f3ab71fcdb4f3fd81fb61de02d9733587c6989bc
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2023-12-22 20:57:51 -0300

    single page

--- wp-content/themes/aquila/single.php
+++ wp-content/themes/aquila/single.php
@@ -1,15 +1,51 @@
 <?php
 /**
- * Main template file.
+ * Single post template file.
  * 
  * @pachkage Aquila
  */
+
 get_header();
 
 ?>
 
-<div class="content">
-    <?php esc_html_e( 'Single post', 'aquila' ); ?>
-</div>
+    <div id="primary">
+        <main id="main" class="site-main mt-5" role="main">
+            <?php
+            if (have_posts()) :
+                ?>
+                <div class="container">
+                    <?php
+                    if (is_home() && !is_front_page()) {
+                        ?>
+                        <header class="mb-5">
+                            <h1 class="page-title screen-reader-text">
+                                <?php single_post_title(); ?>
+                            </h1>
+                        </header>
+                        <?php
+                    }
 
-<?php get_footer(); ?>
+                    while ( have_posts() ) : the_post();
+                    
+                        get_template_part( 'template-parts/content' );
+
+  
+                    endwhile;
+                    ?>
+
+                </div>
+            <?php
+
+            else :
+
+                get_template_part( 'template-parts/content-none' );
+
+            endif;
+            ?>
+        </main>
+    </div>
+
+<?php 
+
+get_footer(); 
