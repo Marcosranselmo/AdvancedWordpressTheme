1fe5dd0b1e18848f7885dea75887a209bf2baa4c
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2023-12-21 22:19:31 -0300

    wp_trim_excerpt

---
+++ wp-content/themes/aquila/template-parts/components/blog/entry-content.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template for entry content.
+ * 
+ * To be used inside WordPress The Loop.
+ * 
+ * @pachkage Aquila
+ */
+
+?>
+
+<div class="entry-content">
+    <?php 
+    if ( is_single() ) {
+        the_content(
+            sprintf(
+                wp_kses(
+                        __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'aquila' ),
+                    [
+                        'span' => [
+                            'class' => []
+                        ]
+                    ]
+                ),
+                the_title( '<span class="screen-reader-text">"', '"</span>', false )
+
+            )
+        );
+    } else {
+        aquila_the_excerpt( 200 );
+    }
+    ?>
+</div>
