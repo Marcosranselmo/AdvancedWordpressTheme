0045e42c38c4bc4e7fe5031b1943a130d4086fea
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2023-12-27 13:25:51 -0300

    register_sidebars_footer

--- wp-content/themes/aquila/footer.php
+++ wp-content/themes/aquila/footer.php
@@ -6,7 +6,18 @@
  */
 ?>
 
-<footer>Footer</footer>
+<footer>
+    <h3>Footer</h3>
+    <?php
+    if ( is_active_sidebar( 'sidebar-2' ) ) {
+        ?>
+            <aside>
+                <?php dynamic_sidebar( 'sidebar-2' ) ?>
+            </aside>
+            <?php
+    }
+    ?>
+</footer>
 </div>
 </div>
 
