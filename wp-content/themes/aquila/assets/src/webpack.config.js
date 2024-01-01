6330f82599c0a523f94cf2b158acb76db84d5182
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-01-01 11:26:30 -0300

    config_webpack

---
+++ wp-content/themes/aquila/assets/src/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require( '.bin' )
+
+const JS_DIR = path
+
+const entry = [
+    main:
+];
+const output = [];
+
+module.exports = ( env, argv ) => ({
+    entry: entry,
+    output: output,
+});
