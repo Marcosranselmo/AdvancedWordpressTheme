b56f26c3b27d6d6e686aa580a1f21e61e6fef30e
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-01-01 16:40:09 -0300

    webpack-rules

--- wp-content/themes/aquila/assets/src/webpack.config.js
+++ wp-content/themes/aquila/assets/src/webpack.config.js
@@ -1,13 +1,53 @@
-const path = require( '.bin' )
+const path = require( 'path' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 
-const JS_DIR = path
+const JS_DIR = path.resolve( __dirname, '/src/js' );
+const IMG_DIR = path.resolve( __dirname, '/src/img' );
+const BUILD_DIR = path.resolve( __dirname, 'build' );
 
-const entry = [
-    main:
+const entry = {
+    main: JS_DIR + '/main.js',
+    single: JS_DIR + '/single.js',
+};
+const output = {
+    path: BUILD_DIR,
+    filename: 'js/[name].js'
+};
+
+const rules = [
+    {
+        test: /\.js$/,
+        include: [ JS_DIR ],
+        exclude: /node_modules/,
+        use:'babel-loader'
+    },
+    {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader'
+        ],
+    },
+    {
+        test: /\.(png|jpg|svg|jpeg|gif|ico|)$/,
+        use: [
+            {
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]',
+                    publicPath: 'production' === process.env.NODE_ENV ? '../' : '../../',
+                },
+            },
+        ],
+    },
 ];
-const output = [];
 
 module.exports = ( env, argv ) => ({
     entry: entry,
     output: output,
+    devtool: 'source-map',
+    module: {
+        rules: rules,
+    }
 });
