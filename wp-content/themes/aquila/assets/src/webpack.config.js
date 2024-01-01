bfa82ba199f13cda48c32d4c2d9ec00638d9155a
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-01-01 17:00:04 -0300

    webpack-rules

--- wp-content/themes/aquila/assets/src/webpack.config.js
+++ wp-content/themes/aquila/assets/src/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require( 'path' );
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const CleanWebpackPlugin = require( 'clean-webpack-plugin' );
 
 const JS_DIR = path.resolve( __dirname, '/src/js' );
 const IMG_DIR = path.resolve( __dirname, '/src/img' );
@@ -43,11 +44,23 @@ const rules = [
     },
 ];
 
+const plugins = ( argv ) => [
+    new CleanWebpackPlugin({
+        cleanStaleWebpackAssets: ( 'production' === argv.mode )
+    }),
+    
+    new MiniCssExtractPlugin({
+        filename: 'css/[name].css'
+    })
+
+];
+
 module.exports = ( env, argv ) => ({
     entry: entry,
     output: output,
     devtool: 'source-map',
     module: {
         rules: rules,
-    }
+    },
+    plugins: plugins( argv ),
 });
