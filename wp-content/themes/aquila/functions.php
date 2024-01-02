70dd73c8f293a57be6b093715093e8e56cd82c7c
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-01-02 17:40:38 -0300

    UglifyJsPlugin

--- wp-content/themes/aquila/functions.php
+++ wp-content/themes/aquila/functions.php
@@ -6,12 +6,36 @@
  * @pachkage Aquila
  */
 
-if ( ! defined( 'AQUILA_DIR_PATH' ) ) {
-    define( 'AQUILA_DIR_PATH', untrailingslashit( get_template_directory() ) );
+
+
+
+
+ if ( ! defined( 'AQUILA_DIR_URI' ) ) {
+    define( 'AQUILA_DIR_URI', untrailingslashit( get_template_directory() ) );
 }
 
-if ( ! defined( 'AQUILA_DIR_URI' ) ) {
-    define( 'AQUILA_DIR_URI', untrailingslashit( get_template_directory_uri() ) );
+if ( ! defined( 'AQUILA_BUILD_URI' ) ) {
+    define( 'AQUILA_BUILD_URI', untrailingslashit( get_template_directory_uri() ) . '/build' );
+}
+
+if ( ! defined( 'AQUILA_BUILD_JS_URI' ) ) {
+    define( 'AQUILA_BUILD_JS_URI', untrailingslashit( get_template_directory_uri() ) . '/build/js' );
+}
+
+if ( ! defined( 'AQUILA_BUILD_JS_DIR_PATH' ) ) {
+    define( 'AQUILA_BUILD_JS_DIR_PATH', untrailingslashit( get_template_directory_uri() ) . '/build/js' );
+}
+
+if ( ! defined( 'AQUILA_BUILD_IMG_URI' ) ) {
+    define( 'AQUILA_BUILD_IMG_URI', untrailingslashit( get_template_directory_uri() ) . '/build/src/img' );
+}
+
+if ( ! defined( 'AQUILA_BUILD_CSS_URI' ) ) {
+    define( 'AQUILA_BUILD_CSS_URI', untrailingslashit( get_template_directory_uri() ) . '/build/css' );
+}
+
+if ( ! defined( 'AQUILA_BUILD_CSS_DIR_PATH' ) ) {
+    define( 'AQUILA_BUILD_CSS_DIR_PATH', untrailingslashit( get_template_directory_uri() ) . '/build/css' );
 }
 
 require_once AQUILA_DIR_PATH . '/inc/helpers/autoloader.php';
